da7d265c4cb50df3f0a81ed5b3437788313a311d
Sujaul Miah <sujaul.m@gmail.com>
2019-10-02 12:12:51 +0100

    initial commit

---
+++ pizza_controller.rb
@@ -0,0 +1,30 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative("./models/pizza_order")
+also_reload("./models/*")
+
+#INDEX ROUTE
+get "/pizza-orders" do
+  @orders = PizzaOrder.all()
+  erb(:index)
+end
+
+#NEW ROUTE - a static part of the url has to be above anything dynamic
+get "/pizza-orders/new" do
+  erb(:new)
+end
+
+#CREATE ROUTE
+post "/pizza-orders" do
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb(:create)
+end
+
+#SHOW ROUTE
+get "/pizza-orders/:id" do
+  @order = PizzaOrder.find(params["id"])
+  erb(:show)
+end
